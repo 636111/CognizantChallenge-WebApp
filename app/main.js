0e64e4029f40eda396c4a946efadd882423e0471
Carmen Dominguez <MariaDelCarmen.DominguezCampillo@cognizant.com>
2017-09-06 15:02:46 +0200

    Add ranking service

--- app/main.js
+++ app/main.js
@@ -2,5 +2,32 @@
 
 angular
   .module('app', [])
-  .controller('mainCtrl', ['$scope', function($scope) {
+  .controller('mainCtrl', ['$scope', 'rankingService', function($scope, rankingService) {
+    
+    $scope.status;
+    $scope.ranking;
+    getRanking();
+    
+    function getRanking() {
+      rankingService.getRanking()
+        .then(function(response) {
+          $scope.ranking = response.data;
+          console.log(JSON.stringify($scope.ranking));
+        }, function(error) {
+          $scope.status = 'Unable to load ranking data: ' + error.message;
+        });
+    };
+
+  }])
+  .factory('rankingService', ['$http', function($http) {
+    
+    var urlBase = "https://cognizantchallenge.herokuapp.com/score",
+        token = '',
+        rankingData = {};
+    
+    rankingData.getRanking = function(){
+      return $http.get(urlBase+'?access_token='+token);
+    };
+
+    return rankingData;
   }]);
