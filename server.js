52898638e326b068502fbb26b055518fced3d2c4
Carmen <MariaDelCarmen.DominguezCampillo@cognizant.com>
2017-09-12 11:51:18 +0200

    heroku init

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     app = express(),
-    port = 3000;
+    port = process.env.PORT || 5000;
 
     //Set up cross origin resource
     app.use(function (req, res, next) {
