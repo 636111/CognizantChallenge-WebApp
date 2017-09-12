7e4240ed86bf2c6faef5751f9651e7a5d750883b
Carmen <MariaDelCarmen.DominguezCampillo@cognizant.com>
2017-09-12 11:57:48 +0200

    heroku init

--- server.js
+++ server.js
@@ -4,10 +4,7 @@ var express = require('express'),
 
     //Set up cross origin resource
     app.use(function (req, res, next) {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        next();
+        
     })
 
 app.use(express.static(__dirname + ''));
